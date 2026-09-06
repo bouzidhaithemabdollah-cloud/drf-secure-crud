fbd79225478fd71c6f1f3da7494ce705a4ba2a11
Haithem <bouzidhaithemabdollah@gmail.com>
2026-09-06 11:54:51 +0100

    Feat: Add JWT authentication and secure endpoints against IDOR

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path 
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/",view=views.register_user,name="register"),
     path("users/<uuid:id>/",view=views.get_user,name="getting"),
     path("users/<uuid:id>/delete/",view=views.delete_user,name="deleting"),
-    path("users/<uuid:id>/update/",view=views.update_user,name='update')
+    path("users/<uuid:id>/update/",view=views.update_user,name='update'),
+    path("api/token/",view=TokenObtainPairView.as_view(),name="access token obtian pair"),
+    path("api/refresh/",view=TokenRefreshView.as_view(),name="refresh token")
 
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view ,permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import OhmySerializer
 from .models import Ohmy
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(["POST"])
 def register_user(request):
@@ -13,7 +14,10 @@ def register_user(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_user(request,id):
+    if str(request.user.id) != str(id):
+        return Response({"error" :"You are not the main user"},status=status.HTTP_401_UNAUTHORIZED)
     try:
         user=Ohmy.objects.get(id=id)
         serializer=OhmySerializer(user)
@@ -22,7 +26,10 @@ def get_user(request,id):
         return Response({"error":"user not found man"},status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete_user(request,id):
+    if str(request.user.id) != str(id):
+        return Response({"error" :"You are not the main user"},status=status.HTTP_403_FORBIDDEN)
     try:
         user=Ohmy.objects.get(id=id)
         if user.is_superuser :
@@ -34,7 +41,10 @@ def delete_user(request,id):
         return Response({"error":"user not found man"},status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['PUT'])
+@permission_classes([IsAuthenticated])
 def update_user(request,id):
+    if str(request.user.id) != str(id):
+        return Response({"error" :"You are not the main user"},status=status.HTTP_403_FORBIDDEN)
     try:
         user=Ohmy.objects.get(id=id)
         if user.is_superuser == True:
